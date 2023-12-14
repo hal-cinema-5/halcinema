bf0ab8e2e3a6dfd871e332dbdb9fd18a321742e7
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2023-12-14 13:10:35 +0900

    コメントアウト削除

--- sinema/site/karitouroku1.php
+++ sinema/site/karitouroku1.php
@@ -249,7 +249,7 @@ if (isset($_POST['seki'])) {
     echo '<script>
         setTimeout(function() {
           window.location.href = "index.php";
-        }, 1000); // 3000ミリ秒 = 3秒
+        }, 1000);
       </script>';
 
   ?>
